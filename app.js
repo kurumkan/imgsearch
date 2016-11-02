72cae9de5a1e42605dc21035d868d2bc00927267
kurumkan <joomrise@gmail.com>
2016-11-03 01:06:51 +0800

    added latest route

--- app.js
+++ app.js
@@ -2,17 +2,73 @@ var express= require("express");
 var app = express();
 var utils = require("./lib/utils.js");
 
-var DB = require("./lib/schema.js");
+// Bing Search client
+var bingSearch = new (require('bing.search'))("plEq0NctaKIEfjl0ltpPloLhUQqY6y8dzuPEpqesvL0");
+
+//DB Schema
+var SearchShema = require("./lib/schema.js");
+
 app.disable('x-powered-by');
 app.use(express.static(__dirname + '/public'));
 app.set("view engine", "ejs");
 
-//our db schema
-var ImgSearch = DB.ImgSearch;
-
 app.get("/", function(request, response){		
 	response.render("index");	
 });
+
+//search query route
+app.get("/api/imagesearch/:term", function(request, response){		
+	var term = request.params.term;
+	
+	//if term was provided - create a db entry
+	if(term){		
+		SearchShema.create({term: term}, function(error, data){
+			if(error) utils.handle500(error, response);			
+		});		    
+	}
+	
+	var offset = +request.query.offset;	
+
+	if(!offset)
+		offset=0;		
+	
+	bingSearch.images(term, 
+		{top: 20, skip: offset},
+		function(error, results) {
+			if(error) utils.handle500(error, response);	  	
+			else
+				response.json(results.map(function(entry){
+					return {
+						url: entry.url,
+						snippet: entry.title,
+						thumbnail: entry.thumbnail.url,
+						context: entry.sourceUrl
+					};
+				})); 
+		}
+	);
+});
+
+//recently submitted search strings
+app.get("/api/latest/imagesearch", function(request, response){		
+
+	SearchShema.find({})
+	.sort({'created': -1})
+	.limit(10)
+	.exec(function(error, results) {
+		if(error){
+			utils.handle500(error, response);			
+		}else{
+			response.json(results.map(function(entry){
+	  			return {
+	  				term: entry.term,
+	  				when: entry.when
+	  			}
+	  		})); 
+		}
+	});
+});
+
 
 //404 error handler
 app.use(function(request, response){	
